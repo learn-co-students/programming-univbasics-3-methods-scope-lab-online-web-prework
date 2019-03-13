e5ce6b13dd7e1785833d1fc751b1b73976375cec
Kellye Greene <kellyecodes@gmail.com>
2019-03-13 16:19:39 -0400

    Adds draft

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,18 +1,8 @@
 def mario
-  phrase = "It's-a me, Mario!"
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
-end
-
-def link
-  receive_sword = 'It\'s Dangerous To Go Alone! Take This.'
-  puts receive_sword
-end
-
-def any_phrase(phrase)
-  puts phrase
 end
