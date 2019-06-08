8bc3663f7733779087aeb50ab481df9e2f23a0bc
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-08 07:23:52 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,18 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts "It's-a me, Mario!"
 end
 
 def toadstool
-  puts status
+  puts "Thank You Mario! But Our Princess Is In Another Castle!"
 end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+describe "any_phrase" do
+  it "takes in an argument and puts out the catch phrase" do
+    phrase = "Do A Barrel Roll!"
+    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+  end
