39ff7a015e16d22d6ca8e634f8cf1b69b8f46d94
Sharon Watkins <swatkins876@gmail.com>
2019-07-30 12:08:42 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,3 +6,18 @@ end
 def toadstool
   puts status
 end
+
+def catch_phrase("It's-a me, Mario!") do
+  puts catch_phrase
+end
+
+def toadstool("Thank You Mario! But Our Princess Is In Another Castle!") do
+  puts toadstool
+end
+
+def any_phrase("Do A Barrel Roll!") Do
+  puts any_phrase
+end
+def link("It's Dangerous To Go Alone! Take This.") do
+  puts link 
+end
