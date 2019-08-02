3a8e9e6a2b80bab5d4d096aa5c1c2a8d8b954e72
restful-strategy-4685 <planetlucid@gmail.com>
2019-08-02 17:55:19 +0000

    Done.

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
+    phrase = "It's-a me, Mario!"
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
