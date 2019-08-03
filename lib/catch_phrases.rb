5eb3b5259427e5930c12c5314c51a9a8470f71fb
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-03 14:04:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -20,4 +20,5 @@ end
 
 describe "Hey_You"
   it "takes in an argument and puts out the catch phase"
-    
+    "Hey_you" = "Do a Barrel Roll!"
+    expect{any_phrase("hey_you")}.to output("do a barrell roll!").to_ruby
