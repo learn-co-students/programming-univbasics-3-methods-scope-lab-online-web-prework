68894f87b30414c0dcd7e71477d40f952ac4ca8d
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-04 21:20:41 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,7 +2,8 @@ def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
-
-def toadstool
-  puts status
+describe "link" do
+  it "puts out 'It's Dangerous To Go Alone! Take This.'" do
+    expect{link}.to output("It's Dangerous To Go Alone! Take This.\n").to_stdout
+  end
 end
