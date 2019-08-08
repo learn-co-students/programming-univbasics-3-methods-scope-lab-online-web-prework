027b774c704b40e21eeabaca15cea2518004bbcc
Tandi Samples <tandisamples@gmail.com>
2019-08-08 00:25:47 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,13 +1,12 @@
-
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
-  status = "Thank You Mario! But Our Princess Is In Another Castole!"
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
-end
+  end
 
 def link
   phrase = "It's Dangerous To Go Alone! Take This."
