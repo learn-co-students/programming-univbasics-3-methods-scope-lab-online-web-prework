a8ae9aefacfb116c48ebc6f0ffed66f15ca3cb76
Lauren Dove <elledoveinc@gmail.com>
2019-08-10 02:50:09 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,20 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts phrase = "It's-a me, Mario!"
 end
-
+mario
+  
 def toadstool
-  puts status
+  puts status = "Thank You Mario! But Our Princess Is In Another Castle!"
 end
+toadstool
+
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+link
+
+def any_phrase( phrase="Do A Barrel Roll!")
+  puts phrase
+  end
+  any_phrase
