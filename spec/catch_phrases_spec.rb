d5b85ca9ab0a0aebfc0da2102bf734ef479ca553
Huda Yousif <hjdoski@gmail.com>
2019-08-19 03:33:22 +0000

    Done.

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -1,7 +1,8 @@
-require 'spec_helper'
+'spec_helper'
 
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
+    phrase = "It's-a me, Mario!"
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
