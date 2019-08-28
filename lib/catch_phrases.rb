6ea5d2715aba08c3cab6821b694cb47e34f71051
virtual-frame-7789 <mttwright@gmail.com>
2019-08-28 00:24:27 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,31 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
+  
+  phrase = "It's-a me, Mario!\n"
+  
   puts phrase
+  
+  
 end
 
 def toadstool
+  
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
   puts status
+  
+  
 end
+
+def link
+  ham = "It's Dangerous To Go Alone! Take This."
+  puts ham
+end
+
+def any_phrase(phrase)
+  sentence = phrase
+  puts sentence
+end
+  
+  
+  
