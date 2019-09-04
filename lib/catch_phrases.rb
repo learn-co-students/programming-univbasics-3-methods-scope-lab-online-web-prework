4c60be113ccf18ba3a7be3f241521dd483fc8e76
Michael Waldo <mbwcogic@yahoo.com>
2019-09-04 01:30:19 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,16 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+def mario 
+puts "It's-a me, Mario!"
+expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+
+ end
 end
 
-def toadstool
-  puts status
+
+
+
+
+
+def link
+  
+  puts "It's Dangerous To Go Alone! Take This."
 end
