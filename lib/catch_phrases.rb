0c879aa995f07ec71c59990c75aaf98fbbe4a463
deezy13 <deezy13@gmail.com>
2019-09-05 17:19:38 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,27 @@
+$status = " "
+
+#$phrase = "Do A Barrel Roll!"
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
+
 def toadstool
-  puts status
+  puts $status
 end
+
+def link
+    puts "It's Dangerous To Go Alone! Take This."
+end
+  
+ 
+def any_phrase(phrase = "Do A Barrel Roll!")
+  phrase "Do A Barrel Roll!"
+  puts phrase
+end
+
+link
+any_phrase("yo")
