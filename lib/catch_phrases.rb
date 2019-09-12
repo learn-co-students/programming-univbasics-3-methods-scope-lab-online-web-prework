10a8d19eb3e3efdbff4e5449151b317ee17c2210
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-12 04:37:53 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,6 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts phrase "Thank You Mario! But Our Princess Is In Another Castle!\n").
 end
 
 def toadstool
