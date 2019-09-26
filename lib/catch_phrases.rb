a8249458c43709e5d70a21963d4c95ecb7504f27
Mark Paul <mpaul730@gmail.com>
2019-09-26 20:03:23 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,3 +6,9 @@ end
 def toadstool
   puts status
 end
+
+describe "mario" do
+  it "puts out 'It's-a me, Mario!'" do
+    phrase = "It's-a me, Mario!"
+    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+  end
