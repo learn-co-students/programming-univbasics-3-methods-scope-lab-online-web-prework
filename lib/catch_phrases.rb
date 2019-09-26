17e30e4f835286bb4b49174a113213ddb78a8179
Ryan Lynch <lynchrbusiness@gmail.com>
2019-09-26 22:14:53 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,5 +4,12 @@ def mario
 end
 
 def toadstool
+ status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
