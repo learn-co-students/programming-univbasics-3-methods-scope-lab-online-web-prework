852a0ac4fcd025f3939a1677648afcd78bdbfcf2
congenial-compressor-7742 <tateejt@gmail.com>
2019-09-26 03:06:12 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,18 @@
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase = "It's-a me, Mario!"
 end
 
 def toadstool
-  puts status
+  puts $status
 end
+
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def any_phrase(a)
+  puts [a]
+end
+any_phrase("Do A Barrel Roll!")
