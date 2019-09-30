9c01b68530fe449810d0563bf1298e3ed13adced
Mary Kanu <maryikanu@gmail.com>
2019-09-30 03:48:51 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,27 @@
+# Method Scope Lab
+
+## Learning Goals
+
+- Define a local variable
+- Define a method
+- Define a method that takes an argument
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
+  
+  describe "mario" do
+  it "puts out 'It's-a me, Mario!'" do
+    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+  end
+  
 end
 
 def toadstool
   puts status
+  
+  describe "toadstool" do
+  it "puts out 'Thank You Mario! But Our Princess Is In Another Castle!'" do
+    expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n").to_stdout
+  end
 end
