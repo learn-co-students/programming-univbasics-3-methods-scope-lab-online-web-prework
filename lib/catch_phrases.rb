acab7f401e57c528a8beea984c680785f8b1dbd4
Pam Cairo <pcairo_90@outlook.com>
2019-09-30 11:48:10 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,20 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  status = 'It's-a me, Mario!'
+  puts status
+end
+def phrase
+local_variable = 'It's-a me, mario!'
+puts local_variable
 end
 
 def toadstool
+  status = 'It's-a me, Mario!''
   puts status
+  end
+
+def link
+  local_variable = "It's Dangerous To Go Alone! Take this."
+  puts local_variable
 end
+  
+  
