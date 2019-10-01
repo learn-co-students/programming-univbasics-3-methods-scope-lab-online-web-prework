90e40a6ba3f4becce76cc96f73648fc08381013c
Aubr R <a.renault2019@outlook.com>
2019-10-01 02:10:19 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,13 @@ def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+
+def link
+  phrase = "It's dangerous To Go Alone! Take This."
+  puts phrase"
+end
+
+def any_phrase(phrase)
+  phrase = "Do A Barrel Roll!"
+  puts phrase
+end
