3e42cefaeb3956380cdfb926eab9a709f7e5aec2
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-10-01 05:02:58 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,10 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  status = 'It's a-me, Mario!'
+  puts "It's a-me, Mario!"
 end
 
 def toadstool
-  puts status
+  local_variable = 'Hey Mario! How are you?'
+  puts local_variable
 end
+
