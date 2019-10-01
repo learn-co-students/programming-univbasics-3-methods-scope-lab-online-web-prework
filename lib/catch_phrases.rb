faee372380a75eccc287e5e2fff082727af8fb29
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-01 17:40:30 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,21 @@
 def mario
+  phrase = "It's-a me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  leaving_now = 'It s Dangerous To Go Alone! Take This.'
+  puts leaving_now
+end
+
+def any_phrase ("do a barrel rool!\n ")
+  phrase = i love rokhaya
+  puts 'phrase'
+end 
+any_phrase
