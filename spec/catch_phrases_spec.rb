41e92df0c9f04214caff6fce0529032551ee8c06
Rick Lim <rick.jason.a.lim@gmail.com>
2019-10-22 17:41:14 +0000

    Done.

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -2,18 +2,21 @@ require 'spec_helper'
 
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
+    phrase = "It's-a me, Mario!"
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
 
 describe "toadstool" do
   it "puts out 'Thank You Mario! But Our Princess Is In Another Castle!'" do
+    status = "Thank You Mario! But Our Princess Is In Another Castle!"
     expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n").to_stdout
   end
 end
 
 describe "link" do
   it "puts out 'It's Dangerous To Go Alone! Take This.'" do
+    shouting = "It's Dangerous To Go Alone! Take This."
     expect{link}.to output("It's Dangerous To Go Alone! Take This.\n").to_stdout
   end
 end
