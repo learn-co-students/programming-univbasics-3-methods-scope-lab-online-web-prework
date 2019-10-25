efee745f4be265b468b7ae8cc985bf20dff9aef8
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-10-25 16:37:24 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,12 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
 
-def toadstool
-  puts status
-end
+
+  def mario
+   phrase = 'It's-a me, Mario !'
+   puts phrase
+  end
+
+  def toadstool
+    status = 'Thank You Mario! But Our Princess Is In Another Castle'
+   puts status
+  end
+
