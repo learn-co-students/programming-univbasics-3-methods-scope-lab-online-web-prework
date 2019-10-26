301eaecc02e2393fcc067b0aa9a125e2f50bc538
merry-attribute-0729 <adamdsmyth@gmail.com>
2019-10-26 20:06:35 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,15 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
 
-def toadstool
+def mario
+  status = 'It\'s-a me, Mario!'
   puts status
+end
+def toadstool
+    status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
+end
+def link 
+puts "It's Dangerous To Go Alone! Take This."
+end
+def any_phrase (quote)
+puts quote
 end
