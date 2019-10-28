402e3694a047f4ad64a5657e425ed3dffd175688
Ingrid sura <sura.ingrid@gmail.com>
2019-10-28 10:02:46 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,21 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  local_variable=phrase 
   puts phrase
 end
+mario
 
 def toadstool
+  local_variable=status
   puts status
 end
+toadstool
+def link
+  puts message
+end
+link
+def any_phrase
+  local_variable=phrase
+  puts phrase
+end
+any_phrase
