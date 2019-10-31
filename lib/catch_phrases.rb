af4419982c77f03f728d32edd1685acb8320ecf2
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-10-31 18:02:01 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,10 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
+  phrase = "It's-a me, Mario!"
+  return phrase
 end
+
 
 def toadstool
   puts status
