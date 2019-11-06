be3ad32ff1fb6ddb4810a8a0c36a2fee3d37a69b
Carly Larko <carly.larko@gmail.com>
2019-11-06 16:16:43 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,11 @@
 def mario
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
-  status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
