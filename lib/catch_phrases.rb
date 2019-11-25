556bd1892f6c574d9d194062cd27b9a4b28ad506
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-11-25 03:09:09 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,4 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
-
-def toadstool
-  puts status
-end
+{mario} = "It's-a me, Mario!\n"
+{toadstool} = "Thank You Mario! But Our Princess Is In Another Castle!\n"
+{link} = "It's Dangerous To Go Alone! Take This.\n"
+{any_phrase} = "Do A Barrel Roll!\n"
