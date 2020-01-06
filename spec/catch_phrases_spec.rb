dfba32edf39a94cd5e581c142fe6da680bf67f55
Timur Mishiev <timurmishiev@gmail.com>
2020-01-06 19:32:02 +0000

    Done.

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
-describe "mario" do
-  it "puts out 'It's-a me, Mario!'" do
-    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+describe "#catch_phrase" do 
+  it "puts out a catch phrase" do 
+    phrase = "It's-a me, Mario!"
+    expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
 
