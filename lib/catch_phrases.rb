47306f4f3ed01320bb7561eb041885f079632664
Christian Pardo <chrispardopersonal@gmail.com>
2020-02-17 02:53:45 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,18 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts $phrase
 end
 
 def toadstool
-  puts status
+  puts $status
+end
+
+def link 
+  puts $phrase 
+end
+
+def any_phrase(phrase)
+  puts "Do A Barrel Roll!"
+  
+  $phrase
 end
--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
+    $phrase = "It's-a me, Mario!"
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
@@ -14,6 +15,7 @@ end
 
 describe "link" do
   it "puts out 'It's Dangerous To Go Alone! Take This.'" do
+    $phrase = "It's Dangerous To Go Alone! Take This."
     expect{link}.to output("It's Dangerous To Go Alone! Take This.\n").to_stdout
   end
 end
