e342e92fad3d9654454c1170749bf50a06c8c42f
maxwellbenton <maxwellbenton@gmail.com>
2020-02-27 17:24:25 -0500

    modify final test to all_phrases instead of any

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -18,9 +18,10 @@ describe "link" do
   end
 end
 
-describe "any_phrase" do
+describe "all_phrases" do
   it "takes in an argument and puts out the catch phrase" do
-    phrase = "Do A Barrel Roll!"
-    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
+    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
+    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
   end
 end
